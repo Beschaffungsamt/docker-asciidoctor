e579e3afcb1dda1d0b3c390f64a975f03483c731
difi <erlend@klakegg.net>
2016-05-26 19:54:59 +0200

    Copying assets.

--- run.sh
+++ run.sh
@@ -7,3 +7,10 @@ for adoc in $(find . | grep main.adoc$); do
 	asciidoctor $adoc -o /target/$(dirname $adoc)/index.html
 
 done
+
+for assets in $(find . | grep .adocassets$); do
+
+	echo "Assets: $(dirname $assets)"
+	mkdir -p /target/$(dirname $assets)
+	cp -r $(dirname $assets) /target/$(dirname $(dirname $assets))
+done
