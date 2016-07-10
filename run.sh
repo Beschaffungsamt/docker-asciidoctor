746242583e3837ce7d8602db3da938e7fc20c08d
Erlend Klakegg Bergheim <erlend@klakegg.net>
2016-07-10 18:09:10 +0200

    Adding support for PDF.

--- run.sh
+++ run.sh
@@ -5,6 +5,7 @@ for adoc in $(find . | grep main.adoc$); do
 	echo "Document: $adoc"
 	mkdir -p /target/$(dirname $adoc)
 	asciidoctor $adoc -o /target/$(dirname $adoc)/index.html
+	asciidoctor-pdf $adoc -o /target/$(dirname $adoc)/document.pdf
 
 done
 
